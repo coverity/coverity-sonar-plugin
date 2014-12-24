dc0a18a1624056f1b7e1ea473240e94b8af44ec5
Franco Rossi <frossi@coverity.com>
2014-12-24 14:56:59 -0800

    Adding headers to recently introduced classes.

--- src/main/java/org/sonar/plugins/coverity/server/CxxLanguage.java
+++ src/main/java/org/sonar/plugins/coverity/server/CxxLanguage.java
@@ -1,3 +1,14 @@
+/*
+ * Coverity Sonar Plugin
+ * Copyright (c) 2014 Coverity, Inc
+ * support@coverity.com
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html.
+ */
+
 package org.sonar.plugins.coverity.server;
 
 import org.sonar.api.Extension;
