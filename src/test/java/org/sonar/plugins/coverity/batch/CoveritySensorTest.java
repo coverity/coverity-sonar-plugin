2932267b196cb3f5da4ef1d32d3c63e9b16761f2
Joel Briggs <jbriggs@synopsys.com>
2017-03-20 13:17:50 -0600

    Initial test for CoveritySensor.execute method
    Verifies that an issue was added

--- src/test/java/org/sonar/plugins/coverity/batch/CoveritySensorTest.java
+++ src/test/java/org/sonar/plugins/coverity/batch/CoveritySensorTest.java
@@ -18,43 +18,99 @@ import com.coverity.ws.v9.ProjectDataObj;
 import org.junit.Before;
 import org.junit.Test;
 import org.sonar.api.batch.SensorContext;
-import org.sonar.api.batch.fs.FileSystem;
-import org.sonar.api.component.ResourcePerspectives;
-import org.sonar.api.config.Settings;
+import org.sonar.api.batch.fs.internal.DefaultInputFile;
+import org.sonar.api.batch.rule.internal.ActiveRulesBuilder;
+import org.sonar.api.batch.rule.internal.DefaultActiveRules;
+import org.sonar.api.batch.rule.internal.NewActiveRule;
+import org.sonar.api.batch.sensor.internal.DefaultSensorDescriptor;
+import org.sonar.api.batch.sensor.internal.SensorContextTester;
+import org.sonar.api.batch.sensor.issue.Issue;
 import org.sonar.api.measures.Measure;
 import org.sonar.api.measures.Metric;
-import org.sonar.api.profiles.RulesProfile;
-import org.sonar.api.resources.Project;
+import org.sonar.api.rule.RuleKey;
+import org.sonar.plugins.coverity.CoverityPlugin;
 import org.sonar.plugins.coverity.ws.CIMClient;
+import org.sonar.plugins.coverity.ws.CIMClientFactory;
+import org.sonar.plugins.coverity.ws.TestCIMClient;
 
-import java.util.Collections;
+import java.io.File;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class CoveritySensorTest {
-    CoveritySensor sensor;
+    private CoveritySensor sensor;
+    private TestCIMClient testCimClient;
 
     @Before
     public void setUp() throws Exception {
-        sensor = new CoveritySensor();
+        CIMClientFactory mockClientFactory = mock(CIMClientFactory.class);
+        testCimClient = new TestCIMClient();
+        when(mockClientFactory.create(any())).thenReturn(testCimClient);
+        sensor = new CoveritySensor(mockClientFactory);
     }
 
     @Test
-    public void testShouldExecuteOnProject() throws Exception {
-        Project project = mock(Project.class);
-        //assertTrue(sensor.shouldExecuteOnProject(project));
+    public void testDescribe_setsName_repositories_properties() throws Exception {
+        final DefaultSensorDescriptor descriptor = new DefaultSensorDescriptor();
+
+        sensor.describe(descriptor);
+
+        assertEquals(sensor.toString(), descriptor.name());
+        final List<String> expectedRepositories = Arrays.asList(CoverityPlugin.REPOSITORY_KEY + "-java",
+                CoverityPlugin.REPOSITORY_KEY + "-cs",
+                CoverityPlugin.REPOSITORY_KEY + "-c",
+                CoverityPlugin.REPOSITORY_KEY + "-cpp",
+                CoverityPlugin.REPOSITORY_KEY + "-c++");
+        assertEquals(expectedRepositories, descriptor.ruleRepositories());
+        assertEquals(Arrays.asList(CoverityPlugin.COVERITY_PROJECT), descriptor.properties());
     }
 
     @Test
-    public void testGetIssueMessage() throws Exception {
-        //
-    }
+    public void testExecute_savesIssue() throws Exception {
 
-    @Test
-    public void testGetCheckerProperties() throws Exception {
-        //
+        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
+        final String filePath = "src/Foo.java";
+        final DefaultInputFile inputFile = new DefaultInputFile("myProjectKey", filePath)
+                .setLanguage("java")
+                .initMetadata("public class Foo {\n}");
+        sensorContextTester
+                .fileSystem()
+                .add(inputFile);
+        final HashMap<String, String> properties = new HashMap<>();
+
+        final String projectName = "my-cov-project";
+        testCimClient.setupProject(projectName);
+
+        properties.put(CoverityPlugin.COVERITY_PROJECT, projectName);
+        properties.put(CoverityPlugin.COVERITY_ENABLE, "true");
+        sensorContextTester
+                .settings()
+                .addProperties(properties);
+
+        final String checkerName = "TEST_CHECKER";
+        final String domain = "STATIC_JAVA";
+
+        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
+        final RuleKey ruleKey = RuleKey.of("coverity-java", domain + "_" + checkerName);
+        final NewActiveRule javaTestChecker = rulesBuilder.create(ruleKey);
+        sensorContextTester
+                .setActiveRules(new DefaultActiveRules(Arrays.asList(javaTestChecker)));
+
+        testCimClient.setupDefect(domain, checkerName, filePath);
+
+        sensor.execute(sensorContextTester);
+
+        final Collection<Issue> issues = sensorContextTester.allIssues();
+        assertNotNull(issues);
+        assertEquals(1, issues.size());
+        final Issue issue = issues.iterator().next();
+        assertEquals(ruleKey, issue.ruleKey());
+        assertEquals(inputFile, issue.primaryLocation().inputComponent());
     }
 
     @Test
@@ -92,11 +148,6 @@ public class CoveritySensorTest {
 
         EventDataObj result = sensor.getMainEvent(dido);
         assertEquals("Found wrong event", em, result);
-    }
-
-    @Test
-    public void testGetResourceForFile() throws Exception {
-        //
     }
 
     @Test
