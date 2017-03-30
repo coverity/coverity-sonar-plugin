2d0d7f13ab927582ffd57ee7617828f1a8f8758a
Andrew Cho <acho@synopsys.com>
2017-03-30 10:48:01 -0600

    Refactoring the unit test method for testing of finding active rule.

--- src/test/java/org/sonar/plugins/coverity/batch/CoveritySensorTest.java
+++ src/test/java/org/sonar/plugins/coverity/batch/CoveritySensorTest.java
@@ -209,237 +209,39 @@ public class CoveritySensorTest {
     }
 
     @Test
-    public void testfindActiveRule_NoneSubcategory() throws Exception {
+    public void testFindActiveRule() throws Exception {
+        // Java
+        verifyFindActiveRule("TEST_CHECKER", "STATIC_JAVA", "coverity-java", "coverity-java", "", "java");
+        verifyFindActiveRule("TEST_CHECKER", "STATIC_JAVA", "coverity-java", "TEST_CHECKER_none", "testSubcategory", "java");
 
-        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
+        // C#
+        verifyFindActiveRule("MSVSCA.TestParseWarning", "STATIC_CS", "coverity-cs", "MSVSCA.*", "", "cs");
+        verifyFindActiveRule("TEST_CHECKER", "STATIC_CS", "coverity-cs", "coverity-cs", "", "cs");
 
-        final String checkerName = "TEST_CHECKER";
-        final String domain = "STATIC_JAVA";
-        final String subcategory = "testSubcategory";
+        // C++ (MISRA, ParseWarning, SemanticWarning, RecoveryWarning)
+        verifyFindActiveRule("MISRA C RULE 10", "STATIC_C", "coverity-cpp", "MISRA.*", "", "cpp");
+        verifyFindActiveRule("PW.TestParseWarning", "STATIC_C", "coverity-cpp", "PW.*", "", "cpp");
+        verifyFindActiveRule("SW.TestParseWarning", "STATIC_C", "coverity-cpp", "SW.*", "", "cpp");
+        verifyFindActiveRule("RW.TestParseWarning", "STATIC_C", "coverity-cpp", "RW.*", "", "cpp");
+        verifyFindActiveRule("TEST_CHECKER", "STATIC_C", "coverity-cpp", "coverity-cpp", "", "cpp");
 
-        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-java", domain + "_" + checkerName + "_none");
-        final NewActiveRule javaTestChecker = rulesBuilder.create(ruleKey);
-        sensorContextTester
-                .setActiveRules(new DefaultActiveRules(Arrays.asList(javaTestChecker)));
-
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, subcategory, "java");
-        assertEquals("STATIC_JAVA_TEST_CHECKER_none", rule.ruleKey().rule());
+        // JavaScript, Python, PHP
+        verifyFindActiveRule("JSHINT.TestParseWarning", "OTHER", "coverity-js", "JSHINT.*", "", "js");
+        verifyFindActiveRule("TEST_CHECKER", "OTHER", "coverity-js", "coverity-js", "", "js");
+        verifyFindActiveRule("TEST_CHECKER", "OTHER", "coverity-py", "coverity-py", "", "py");
+        verifyFindActiveRule("TEST_CHECKER", "OTHER", "coverity-php", "coverity-php", "", "php");
     }
 
-    @Test
-    public void testfindActiveRule_GeneralMisra() throws Exception {
-
+    private void verifyFindActiveRule(String checkerName, String domain, String repoKey, String key, String subcategory, String lang) throws Exception {
         final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
 
-        final String checkerName = "MISRA C RULE 10";
-        final String domain = "STATIC_C";
-
         final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-cpp", domain + "_MISRA.*");
-        final NewActiveRule misraChecker = rulesBuilder.create(ruleKey);
-        sensorContextTester
-                .setActiveRules(new DefaultActiveRules(Arrays.asList(misraChecker)));
-
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, "", "cpp");
-        assertEquals("STATIC_C_MISRA.*", rule.ruleKey().rule());
-    }
-
-    @Test
-    public void testfindActiveRule_ParseWarning() throws Exception {
-
-        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
-
-        final String checkerName = "PW.TestParseWarning";
-        final String domain = "STATIC_C";
-
-        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-cpp", domain + "_PW.*");
-        final NewActiveRule pwChecker = rulesBuilder.create(ruleKey);
-        sensorContextTester
-                .setActiveRules(new DefaultActiveRules(Arrays.asList(pwChecker)));
-
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, "", "cpp");
-        assertEquals("STATIC_C_PW.*", rule.ruleKey().rule());
-    }
-
-    @Test
-    public void testfindActiveRule_SemanticWarning() throws Exception {
-
-        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
-
-        final String checkerName = "SW.TestParseWarning";
-        final String domain = "STATIC_C";
-
-        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-cpp", domain + "_SW.*");
-        final NewActiveRule swChecker = rulesBuilder.create(ruleKey);
-        sensorContextTester
-                .setActiveRules(new DefaultActiveRules(Arrays.asList(swChecker)));
-
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, "", "cpp");
-        assertEquals("STATIC_C_SW.*", rule.ruleKey().rule());
-    }
-
-    @Test
-    public void testfindActiveRule_RecoveryWarning() throws Exception {
-
-        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
-
-        final String checkerName = "RW.TestParseWarning";
-        final String domain = "STATIC_C";
-
-        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-cpp", domain + "_RW.*");
-        final NewActiveRule rwChecker = rulesBuilder.create(ruleKey);
-        sensorContextTester
-                .setActiveRules(new DefaultActiveRules(Arrays.asList(rwChecker)));
-
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, "", "cpp");
-        assertEquals("STATIC_C_RW.*", rule.ruleKey().rule());
-    }
-
-    @Test
-    public void testfindActiveRule_GeneralCpp() throws Exception {
-
-        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
-
-        final String checkerName = "TEST_CHECKER";
-        final String domain = "STATIC_C";
-
-        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-cpp", domain + "_coverity-cpp");
-        final NewActiveRule rwChecker = rulesBuilder.create(ruleKey);
-        sensorContextTester
-                .setActiveRules(new DefaultActiveRules(Arrays.asList(rwChecker)));
-
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, "", "cpp");
-        assertEquals("STATIC_C_coverity-cpp", rule.ruleKey().rule());
-    }
-
-    @Test
-    public void testfindActiveRule_Msvsca() throws Exception {
-
-        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
-
-        final String checkerName = "MSVSCA.TestParseWarning";
-        final String domain = "STATIC_CS";
-
-        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-cs", domain + "_MSVSCA.*");
-        final NewActiveRule csChecker = rulesBuilder.create(ruleKey);
-        sensorContextTester
-                .setActiveRules(new DefaultActiveRules(Arrays.asList(csChecker)));
-
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, "", "cs");
-        assertEquals("STATIC_CS_MSVSCA.*", rule.ruleKey().rule());
-    }
-
-    @Test
-    public void testfindActiveRule_GeneralCs() throws Exception {
-
-        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
-
-        final String checkerName = "TEST_CHECKER";
-        final String domain = "STATIC_CS";
-
-        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-cs", domain + "_coverity-cs");
-        final NewActiveRule csChecker = rulesBuilder.create(ruleKey);
-        sensorContextTester
-                .setActiveRules(new DefaultActiveRules(Arrays.asList(csChecker)));
-
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, "", "cs");
-        assertEquals("STATIC_CS_coverity-cs", rule.ruleKey().rule());
-    }
-
-    @Test
-    public void testfindActiveRule_GeneralJava() throws Exception {
-
-        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
-
-        final String checkerName = "TEST_CHECKER";
-        final String domain = "STATIC_JAVA";
-
-        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-java", domain + "_coverity-java");
-        final NewActiveRule csChecker = rulesBuilder.create(ruleKey);
-        sensorContextTester
-                .setActiveRules(new DefaultActiveRules(Arrays.asList(csChecker)));
-
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, "", "java");
-        assertEquals("STATIC_JAVA_coverity-java", rule.ruleKey().rule());
-    }
-
-    @Test
-    public void testfindActiveRule_Jshint() throws Exception {
-
-        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
-
-        final String checkerName = "JSHINT.TestParseWarning";
-        final String domain = "OTHER";
-
-        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-js", domain + "_JSHINT.*");
-        final NewActiveRule jsChecker = rulesBuilder.create(ruleKey);
-        sensorContextTester
-                .setActiveRules(new DefaultActiveRules(Arrays.asList(jsChecker)));
-
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, "", "js");
-        assertEquals("OTHER_JSHINT.*", rule.ruleKey().rule());
-    }
-
-    @Test
-    public void testfindActiveRule_GeneralJavaScript() throws Exception {
-
-        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
-
-        final String checkerName = "TEST_CHECKER";
-        final String domain = "OTHER";
-
-        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-js", domain + "_coverity-js");
-        final NewActiveRule jsChecker = rulesBuilder.create(ruleKey);
-        sensorContextTester
-                .setActiveRules(new DefaultActiveRules(Arrays.asList(jsChecker)));
-
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, "", "js");
-        assertEquals("OTHER_coverity-js", rule.ruleKey().rule());
-    }
-
-    @Test
-    public void testfindActiveRule_GeneralPython() throws Exception {
-
-        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
-
-        final String checkerName = "TEST_CHECKER";
-        final String domain = "OTHER";
-
-        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-py", domain + "_coverity-py");
+        final RuleKey ruleKey = RuleKey.of(repoKey, domain + "_" + key);
         final NewActiveRule pyChecker = rulesBuilder.create(ruleKey);
         sensorContextTester
                 .setActiveRules(new DefaultActiveRules(Arrays.asList(pyChecker)));
 
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, "", "py");
-        assertEquals("OTHER_coverity-py", rule.ruleKey().rule());
-    }
-
-    @Test
-    public void testfindActiveRule_GeneralPhp() throws Exception {
-
-        final SensorContextTester sensorContextTester = SensorContextTester.create(new File("src"));
-
-        final String checkerName = "TEST_CHECKER";
-        final String domain = "OTHER";
-
-        final ActiveRulesBuilder rulesBuilder = new ActiveRulesBuilder();
-        final RuleKey ruleKey = RuleKey.of("coverity-php", domain + "_coverity-php");
-        final NewActiveRule pyChecker = rulesBuilder.create(ruleKey);
-        sensorContextTester
-                .setActiveRules(new DefaultActiveRules(Arrays.asList(pyChecker)));
-
-        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, "", "php");
-        assertEquals("OTHER_coverity-php", rule.ruleKey().rule());
+        ActiveRule rule = sensor.findActiveRule(sensorContextTester, domain, checkerName, subcategory, lang);
+        assertEquals(domain + "_" + key, rule.ruleKey().rule());
     }
 }
