725fd79a92f8588d771b4335c3344927932345f8
Margaret Fendall <smlin@blackduck.com>
2024-12-19 10:41:46 -0800

    update copyright

--- src/main/java/org/sonar/plugins/coverity/batch/CoveritySensor.java
+++ src/main/java/org/sonar/plugins/coverity/batch/CoveritySensor.java
@@ -1,6 +1,6 @@
 /*
  * Coverity Sonar Plugin
- * Copyright (c) 2021 Synopsys, Inc
+ * Copyright 2024 Black Duck Software, Inc. All rights reserved.
  *
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
