2827aeeaa1489530afdeda0db7a6a03b43a1800d
Margaret Fendall <smlin@blackduck.com>
2024-12-19 10:41:32 -0800

    remove support email

--- src/test/java/org/sonar/plugins/coverity/CoverityPluginTest.java
+++ src/test/java/org/sonar/plugins/coverity/CoverityPluginTest.java
@@ -1,7 +1,6 @@
 /*
  * Coverity Sonar Plugin
  * Copyright (c) 2021 Synopsys, Inc
- * support@coverity.com
  *
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
