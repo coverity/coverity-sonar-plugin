020dc2ee3c5dc62b29959ea6a2f22028326e6e1e
Margaret Fendall <smlin@blackduck.com>
2024-12-19 11:23:57 -0800

    update one more copyright

--- build.py
+++ build.py
@@ -1,12 +1,12 @@
 #/*******************************************************************************
-# * Copyright (c) 2017 Synopsys, Inc
+# * Copyright 2024 Black Duck Software, Inc. All rights reserved.
 # * All rights reserved. This program and the accompanying materials
 # * are made available under the terms of the Eclipse Public License v1.0
 # * which accompanies this distribution, and is available at
 # * http://www.eclipse.org/legal/epl-v10.html
 # *
 # * Contributors:
-# *    Synopsys, Inc - initial implementation and documentation
+# *    Black Duck Software, Inc - initial implementation and documentation
 # *******************************************************************************/
 import sys
 import subprocess
